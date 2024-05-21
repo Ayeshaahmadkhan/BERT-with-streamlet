9093387a484a3484457e6f9653d8cc60c956da46
Ayesha Ahmad <ayeshaahmadkhan8827@gmail.com>
2024-05-21 19:34:24 +0500

    Add initial app.py and virtual environment

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
+import torch
+
+
+# Load model and tokenizer
+model_name = "deepset/roberta-base-squad2"
+model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+# Set up the QA pipeline
+nlp = pipeline('question-answering', model=model, tokenizer=tokenizer)
+
+st.title("Question Answering with BERT")
+
+
+# Input fields for question and context
+
+question = st.text_input("Enter your question:")
+context = st.text_area("Enter the context:")
+
+# If the user has entered both a question and context, process the input
+if question and context:
+    QA_input = {
+        "question": question,
+        "context": context
+    }
+    result = nlp(QA_input)
+    st.write(f"**Question:** {question}")
+    st.write(f"**Answer:** {result['answer']}")
+
+
